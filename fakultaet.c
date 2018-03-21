6447b529768c7e1a65d534631176f55ff4092b58
Wolfgang Folzwinkler <csat6481@zid-t9pc1905.uibk.ac.at>
2018-03-21 15:54:49 +0100

    fakultaet.c erstellt

---
+++ fakultaet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+
+int b; 
+int c=1; 
+int a;
+        a = atof(argv[1]);
+if(a>0){
+
+b=a;
+while(b>0){
+c=c*b;
+b--;
+}
+printf("Fakultät von %i ist gleich %i.\n", a , c);
+} 
+else if (a==0){
+printf("Fakultät von %i ist gleich 1.\n", a);
+}
+else{ 
+printf("Geben Sie bitte nur Zahlen größer gleich 0 ein.\n");
+}
+//Ab Zahlen größer 12 funktioniert dieses Programm nicht mehr!
+}
