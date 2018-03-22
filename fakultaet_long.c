345b5fe938a7efb4d6d55d3d79e870bcd3c3091a
Wolfgang Folzwinkler <csat6481@zid-t9pc1919.uibk.ac.at>
2018-03-22 10:28:45 +0100

    fakultaet_long erstellt

---
+++ fakultaet_long.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv) {
+
+int b;
+long c=1; 
+int a;
+
+        a = atoi(argv[1]);
+if(a>0){
+b=a;
+while(b>0){
+c=c*b;
+b--;
+}
+printf("Fakultät von %i ist gleich %ld.\n", a , c);
+} 
+else if (a==0){
+printf("Fakultät von %i ist gleich 1.\n", a);
+}
+else{ 
+printf("Geben Sie bitte nur Zahlen größer gleich 0 ein.\n");
+}
+//Ab Zahlen größer 20 funktioniert dieses Programm nicht mehr!
+}
